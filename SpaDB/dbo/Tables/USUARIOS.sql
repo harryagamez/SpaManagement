cb060c861f05ac2025305e025d3e70909daf5563
Pablo Quiroga <pabloquiroga13@gmail.com>
2020-04-21 19:59:30 -0500

    Creación proyecto Base de datos en la solución.

---
+++ SpaDB/dbo/Tables/USUARIOS.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[USUARIOS] (
+    [ID_USUARIO]         INT              IDENTITY (1, 1) NOT NULL,
+    [NOMBRE]             CHAR (25)        NULL,
+    [CONTRASEÑA]         CHAR (15)        NULL,
+    [PERFIL]             CHAR (15)        NULL,
+    [ID_EMPRESA]         UNIQUEIDENTIFIER NULL,
+    [CODIGO_INTEGRACION] VARCHAR (36)     NULL,
+    [FECHA_REGISTRO]     DATETIME         NULL,
+    [FECHA_MODIFICACION] DATETIME         NULL,
+    CONSTRAINT [PK_USUARIOS] PRIMARY KEY CLUSTERED ([ID_USUARIO] ASC)
+);
+
