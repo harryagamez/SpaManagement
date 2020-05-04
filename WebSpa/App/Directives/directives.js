7b221d7e5134e710e68127706548cfec2dca16ec
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2020-05-04 18:56:44 -0500

    Padding top al grid clientes Menu activo seleccionado. Cambio del color del menu activo y hover por oscuro. Cambio al borde de la lista de menus para resaltar mejor.

--- WebSpa/App/Directives/directives.js
+++ WebSpa/App/Directives/directives.js
@@ -28,6 +28,29 @@
                     $element.text($filter('date')(new Date(), $attrs.dateNow));
                 }
             };
+        }])
+        .directive('autoActive', ['$location', function ($location) {
+            return {
+                restrict: 'A',
+                scope: false,
+                link: function (scope, element) {
+                    function setActive() {
+                        var path = $location.path();
+                        if (path) {
+                            angular.forEach(element.find('li'), function (li) {
+                                var anchor = li.querySelector('a');
+                                if (anchor.href.match('#' + path + '(?=\\?|$)')) {
+                                    angular.element(li).addClass('active');
+                                } else {
+                                    angular.element(li).removeClass('active');
+                                }
+                            });
+                        }
+                    }
+                    setActive();
+                    scope.$on('$locationChangeSuccess', setActive);
+                }
+            }
         }]);
 
 
