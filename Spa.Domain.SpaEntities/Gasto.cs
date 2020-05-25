deb864e3008e72ec4de116711358cc52a683f306
Pablo Quiroga <pabloquiroga13@gmail.com>
2020-05-25 11:51:13 -0500

    Ajustes proyecto base de datos.

--- Spa.Domain.SpaEntities/Gasto.cs
+++ Spa.Domain.SpaEntities/Gasto.cs
@@ -6,7 +6,6 @@ namespace Spa.Domain.SpaEntities
     public class Gasto : BusquedaGasto
     {
         public int Id_Gasto { get; set; }
-        public string Tipo_Gasto { get; set; }
         public string Descripcion { get; set; }
         public float Valor { get; set; }
         public DateTime? Fecha { get; set; }
@@ -14,6 +13,5 @@ namespace Spa.Domain.SpaEntities
         public int Id_Empleado { get; set; }
         public DateTime? Fecha_Registro { get; set; }
         public DateTime? Fecha_Modificacion { get; set; }
-        public string Id_Empresa { get; set; }
     }
 }
