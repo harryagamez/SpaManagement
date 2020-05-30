64fae67c492c7533c736e28be700f3b8d3be1ee7
Pablo Quiroga <pabloquiroga13@gmail.com>
2020-05-30 10:32:34 -0500

    Cambios eliminar imagen.

--- WebSpa/App/Controllers/Controller.js
+++ WebSpa/App/Controllers/Controller.js
@@ -806,6 +806,7 @@
 
         // Visualizar Imagen
         $scope.VisualizarImagen = function (data) {
+
             $rootScope.ServicioNombre = data.nombre;
             $rootScope.ServicioListaImagenes = [];
             $rootScope.ServicioListaImagenes = data.imagenes_Servicio;
@@ -814,6 +815,7 @@
                 $scope.ModalSliderServicio();
             else
                 $scope.showAlertSinImagenesAdjuntas();
+
         }
 
         // Consultar Servicio Por Nombre
@@ -990,6 +992,7 @@
 
                 }, function () {
                 });
+
         };
         
 
@@ -3046,13 +3049,20 @@
         //  Eliminar Imagen Adjunta Servicio
         $scope.EliminarImagenAdjunta = function (data) {            
             
-            let IdImagenAdjunta = data;
+            let IdImagenAdjunta = data.id_Servicio_Imagen;
 
             SPAService._eliminarImagenAdjunta(IdImagenAdjunta)
                 .then(
                     function (result) {
                         if (result.data === true) {
-                            toastr.success('Imagen eliminada correctamente', '', $scope.toastrOptions);                            
+                            toastr.success('Imagen eliminada correctamente', '', $scope.toastrOptions); 
+
+                            let index = $scope.ServicioImagenesAdjuntas.indexOf(data);
+                            $scope.ServicioImagenesAdjuntas.splice(index, 1);  
+
+                            $rootScope.ImagenesAdjuntas = $scope.ServicioImagenesAdjuntas.length;
+                            $scope.ImagenesAdjuntas = $rootScope.ImagenesAdjuntas;
+
                         }
                     }, function (err) {
                         toastr.remove();
@@ -3076,7 +3086,9 @@
                 .multiple(true);
 
             $mdDialog.show(confirm).then(function () {
+
                 $scope.EliminarImagenAdjunta(data);
+
             }, function () {
                 return;
             });
