a69ed15a52a2f1a25decf8edc61411d537fc06af
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2020-10-15 22:38:09 -0500

    Ajuste

--- Spa.Application.SpaService/ISpaService.cs
+++ Spa.Application.SpaService/ISpaService.cs
@@ -62,6 +62,7 @@ namespace Spa.Application.SpaService
         Task EmailConfirmacionAgenda(Agenda agenda);
         bool RegistrarClientes(List<Cliente> clientes);
         bool RegistrarFacturacionServicios(AplicacionPago aplicacionPago);
+        List<EmpleadoNomina> ConsultarEmpleadosNomina(string idEmpresa, string fechaNomina);
         bool SincronizarDepartamentos(List<DepartmentProperties> _departmentProperties);
     }
 }
