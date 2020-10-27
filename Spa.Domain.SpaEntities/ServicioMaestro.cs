75e74c51dfa0f9898799c1056903c90f9884dc7c
Pablo Quiroga <pabloquiroga13@gmail.com>
2020-10-27 17:31:13 -0500

    Ajuste Entidad - ServicioMaestro

--- Spa.Domain.SpaEntities/ServicioMaestro.cs
+++ Spa.Domain.SpaEntities/ServicioMaestro.cs
@@ -6,9 +6,6 @@ namespace Spa.Domain.SpaEntities
     public class ServicioMaestro : BusquedaServicio
     {
         public int Id_Servicio { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-        public int Id_TipoServicio { get; set; }
         public string Id_Categoria_Servicio { get; set; }
         public DateTime? Fecha_Registro { get; set; }
         public DateTime? Fecha_Modificacion { get; set; }
