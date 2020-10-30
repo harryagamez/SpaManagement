afbfa14c76e3f2af63c3d280dbc9ea687ffbfbf4
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2020-10-30 17:44:09 -0500

    Avance

---
+++ WebSpa/App/Controllers/ServiciosClientesController.js
@@ -0,0 +1,8 @@
+﻿angular.module('app.controllers')
+    .controller("AdministratorController", AdministratorController);
+
+AdministratorController.$inject = ['$scope', '$state', '$location', '$mdDialog', '$rootScope', '$timeout', 'AuthService'];
+
+function AdministratorController($scope, $state, $location, $mdDialog, $rootScope, $timeout, authService) {
+    $rootScope.header = 'SPA Management - Servicios por Cliente';    
+}
