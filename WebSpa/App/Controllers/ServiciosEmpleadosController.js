b965ba4ed33539a260e3f6b8b776c50774fcbcd5
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2020-11-09 16:43:54 -0500

    Commit

---
+++ WebSpa/App/Controllers/ServiciosEmpleadosController.js
@@ -0,0 +1,9 @@
+﻿angular.module('app.controllers')
+    .controller("ServiciosEmpleadosController", ServiciosEmpleadosController);
+
+ServiciosEmpleadosController.$inject = ['$scope', '$rootScope', '$filter', '$mdDialog', '$mdToast', '$document', '$timeout', '$http', 'localStorageService', 'SPAService'];
+
+function ServiciosEmpleadosController($scope, $rootScope, $filter, $mdDialog, $mdToast, $document, $timeout, $http, localStorageService, SPAService) {
+    $rootScope.header = 'Reportes - Servicios por Empleado';
+    $scope.IdEmpresa = $rootScope.Id_Empresa;
+}
