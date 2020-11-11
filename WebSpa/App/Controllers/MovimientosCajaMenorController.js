7e11710475eec457cc80d19b7a97b0f5488653b6
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2020-11-11 10:57:41 -0500

    Staged

---
+++ WebSpa/App/Controllers/MovimientosCajaMenorController.js
@@ -0,0 +1,9 @@
+﻿angular.module('app.controllers')
+    .controller("MovimientosCajaMenorController", MovimientosCajaMenorController);
+
+MovimientosCajaMenorController.$inject = ['$scope', '$rootScope', '$filter', '$mdDialog', '$mdToast', '$document', '$timeout', '$http', 'localStorageService', 'SPAService'];
+
+function MovimientosCajaMenorController($scope, $rootScope, $filter, $mdDialog, $mdToast, $document, $timeout, $http, localStorageService, SPAService) {
+    $rootScope.header = 'Reportes - Movimientos Caja Menor';
+    $scope.IdEmpresa = $rootScope.Id_Empresa;
+}
