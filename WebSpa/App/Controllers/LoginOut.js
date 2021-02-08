7f6b2326b5fd1379b2b059f81e98a8e09165322b
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2021-02-08 15:57:17 -0500

    Ajustes

---
+++ WebSpa/App/Controllers/LoginOut.js
@@ -0,0 +1,8 @@
+﻿angular.module('app.controllers')
+    .controller("LoginOutController", LoginOutController);
+
+LoginOutController.$inject = ['$scope', '$state', '$location', '$rootScope', '$timeout', 'AuthService'];
+
+function LoginController($scope, $state, $location, $rootScope, $timeout, authService) {
+
+}
