2a3d4caedfd8f118de27de5802b0d6e90cca3cd6
Harry Agámez Pedraza <harry.agamez85@gmail.com>
2021-02-10 15:43:21 -0500

    Directiva deshabilitar espacios Login

--- WebSpa/App/Directives/directives.js
+++ WebSpa/App/Directives/directives.js
@@ -62,6 +62,17 @@
                 });
             };
         })
+        .directive('disallowSpaces', function () {
+            return {
+                restrict: 'A',
+
+                link: function ($scope, $element) {
+                    $element.bind('input', function () {
+                        $(this).val($(this).val().replace(/ /g, ''));
+                    });
+                }
+            };
+        })
         .directive('dragMe', function () {
             return {
                 restrict: 'A',
